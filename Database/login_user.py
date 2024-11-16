1f755edcc9dabbbe5286133b5fd1651c82b372d3
Not-MilanGurung <notmilangurung@gmail.com>
2024-11-16 20:03:36 +0545

    Login

--- Database/login_user.py
+++ Database/login_user.py
@@ -1,13 +1,25 @@
 import sqlite3
-from register_user import personal_detail
+
 con = sqlite3.connect("./Database/user.db")
 cur = con.cursor()
 
 # cur.execute("CREATE TABLE login(Id INTEGER PRIMARY KEY AUTOINCREMENT, UserName, Password, Phone, Address, Email, Name)")
 # cur.execute("DROP TABLE login")
-personal_detail(["Milan","Gurung"], "Lalitpur", 123, "hack@gmail")
-res = cur.execute("SELECT * FROM login")
-a = res.fetchall()
-for x in a:
-    print(x)
-con.close()
+
+def login(user_name: str, password: str) -> bool:
+
+    # Checking if the User Name is registered
+    res = cur.execute(f"SELECT Password FROM login WHERE UserName = '{user_name}'")
+    p = res.fetchone()
+    if p is None:
+        print('UserName is not registered')
+        return False
+    
+    # Check if the password matches
+    if p[0] != password:
+        print('Incorrect password')
+        return False
+    
+    # Login if the requirements are met
+    print(f'Welcome, {user_name}')
+    return True
